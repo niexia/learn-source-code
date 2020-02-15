aece0df7635f8704eefe561621c5e159a4b40b46
杨金 <yangjin.im@bytedance.com>
2020-02-16 00:25:28 +0800

    chore: 使用 vuepress 管理文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,104 @@
+module.exports = {
+  title: '源码学习',
+  description: 'Vue | React 源码学习',
+  base: '/learn-source-code/',
+  dest: 'dist',
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/favicon.ico'
+    }]
+  ],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "public", "assets")
+      }
+    }
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    repo: 'niexias/learn-source-code',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    nav: [{
+      text: '个人博客',
+      link: 'https://niexias.github.io/'
+    }],
+    sidebarDepth: 2,
+    sidebar: [{
+      title: "写在最前面",
+      collapsable: false,
+      children: [
+        ["start/", "写在最前面"]
+      ]
+    }, {
+      title: "数据驱动",
+      collapsable: false,
+      children: []
+    }, {
+      title: "响应式",
+      collapsable: false,
+      children: [
+      ]
+    }, {
+      title: "虚拟DOM",
+      collapsable: false,
+      children: [
+      ]
+    }, {
+      title: "模板编译篇",
+      collapsable: false,
+      children: [
+      ]
+    }, {
+      title: "生命周期篇",
+      collapsable: false,
+      children: [
+      ]
+    }, {
+      title: "实例方法篇",
+      collapsable: false,
+      children: [
+      ]
+    }, {
+      title: "全局API篇",
+      collapsable: false,
+      children: [
+        ["globalAPI/", "全局API分析"]
+      ]
+    }, {
+      title: "过滤器篇",
+      collapsable: false,
+      children: [
+      ]
+    }, {
+      title: "指令篇",
+      collapsable: false,
+      children: [
+      ]
+    }, {
+      title: "内置组件篇",
+      collapsable: false,
+      children: [
+      ]
+    }]
+  },
+  plugins: [
+    ['@vuepress/back-to-top', true],
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "文档内容有更新，点击刷新立即查看新内容。",
+          buttonText: "刷新"
+        }
+      }
+    ]
+  ] 
+}
