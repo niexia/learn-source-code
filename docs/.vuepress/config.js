0586007d9c1d619f9dfe17d5caaa66628b625fd7
杨金 <yangjin.im@bytedance.com>
2021-07-27 01:46:46 +0800

    chore: 更新 vuepress 并增加 config 配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,70 +22,98 @@ module.exports = {
     lineNumbers: false
   },
   themeConfig: {
-    repo: 'niexias/learn-source-code',
+    repo: 'niexia/learn-source-code',
     editLinks: true,
     docsDir: 'docs',
     editLinkText: '在 GitHub 上编辑此页',
     lastUpdated: '上次更新',
     nav: [{
-      text: '个人博客',
-      link: 'https://niexias.github.io/'
-    }],
+        text: 'Vue 2',
+        link: '/vue/'
+      },
+      {
+        text: 'Vue 3',
+        link: '/vue-next/'
+      },
+      {
+        text: 'React',
+        link: '/react/'
+      },
+      {
+        text: '个人博客',
+        link: 'https://niexia.github.io/'
+      },
+    ],
     sidebarDepth: 2,
-    sidebar: [{
-      title: "数据驱动",
+    sidebar: {
+      '/vue/': [{
+        title: "数据驱动",
+        collapsable: false,
+        children: [
+          '/vue/instance/',
+          '/vue/instance/Vue是什么',
+          '/vue/instance/newVue初始化'
+        ]
+      }, {
+        title: "响应式",
+        collapsable: false,
+        children: []
+      }, {
+        title: "虚拟DOM",
+        collapsable: false,
+        children: []
+      }, {
+        title: "模板编译篇",
+        collapsable: false,
+        children: []
+      }, {
+        title: "生命周期篇",
+        collapsable: false,
+        children: []
+      }, {
+        title: "实例方法篇",
+        collapsable: false,
+        children: []
+      }, {
+        title: "全局API篇",
+        collapsable: false,
+        children: []
+      }, {
+        title: "过滤器篇",
+        collapsable: false,
+        children: []
+      }, {
+        title: "指令篇",
+        collapsable: false,
+        children: []
+      }, {
+        title: "内置组件篇",
+        collapsable: false,
+        children: []
+      }],
+    },
+    '/vue-next/': [{
+      title: '核心实现',
       collapsable: false,
-      children: [
-        '/vue/instance/',
-        '/vue/instance/Vue是什么',
-        '/vue/instance/newVue初始化'
-      ]
+      sidebarDepth: 2,
+      children: []
     }, {
-      title: "响应式",
+      title: 'Composition API',
       collapsable: false,
-      children: [
-      ]
+      sidebarDepth: 2,
+      children: []
     }, {
-      title: "虚拟DOM",
+      title: '优化思想',
       collapsable: false,
-      children: [
-      ]
+      sidebarDepth: 2,
+      children: []
     }, {
-      title: "模板编译篇",
+      title: '总结对比',
       collapsable: false,
-      children: [
-      ]
-    }, {
-      title: "生命周期篇",
-      collapsable: false,
-      children: [
-      ]
-    }, {
-      title: "实例方法篇",
-      collapsable: false,
-      children: [
-      ]
-    }, {
-      title: "全局API篇",
-      collapsable: false,
-      children: [
-      ]
-    }, {
-      title: "过滤器篇",
-      collapsable: false,
-      children: [
-      ]
-    }, {
-      title: "指令篇",
-      collapsable: false,
-      children: [
-      ]
-    }, {
-      title: "内置组件篇",
-      collapsable: false,
-      children: [
-      ]
-    }]
+      sidebarDepth: 2,
+      children: []
+    }],
+    '/react/': []
   },
   plugins: [
     ['@vuepress/back-to-top', true],
@@ -99,5 +127,5 @@ module.exports = {
         }
       }
     ]
-  ] 
+  ]
 }
